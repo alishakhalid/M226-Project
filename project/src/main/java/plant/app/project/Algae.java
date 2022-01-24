a6d784916a473774449dec9d7c535f6f46bcf168
blerine <blerine.berisha@noseryoung.ch>
2022-01-24 22:01:36 +0100

    base structure, added javafx

---
+++ project/src/main/java/plant/app/project/Algae.java
@@ -0,0 +1,4 @@
+package plant.app.project;
+
+public class Algae {
+}
---
+++ project/src/main/java/plant/app/project/HelloController.java
@@ -0,0 +1,9 @@
+package plant.app.project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+
+public class HelloController {
+
+}
